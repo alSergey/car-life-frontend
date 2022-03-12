ae287427274f7097eaef214b166538e0d6efa4d2
Alekhin Sergey <seralekhin@ozon.ru>
2022-03-12 12:20:40 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { View, Panel, PanelHeader, Group, Div } from '@vkontakte/vkui';
+import {
+    AppRoot,
+    SplitLayout,
+    PanelHeader,
+    SplitCol,
+    View,
+    Panel,
+    Group,
+    Header,
+    SimpleCell,
+    useAdaptivity, ViewWidth
+} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 const App: React.FC = () => {
+    const { viewWidth } = useAdaptivity();
+
 	return (
-		<View activePanel="mainPanel">
-            <Panel id="mainPanel">
-                <PanelHeader>Example</PanelHeader>
-                <Group>
-                    <Div>
-                        Hello world
-                    </Div>
-                </Group>
-            </Panel>
-        </View>
+        <AppRoot>
+            <SplitLayout header={<PanelHeader separator={false} />}>
+                <SplitCol spaced={viewWidth && viewWidth > ViewWidth.MOBILE}>
+                    <View activePanel="main">
+                        <Panel id="main">
+                            <PanelHeader>CarLife</PanelHeader>
+                            <Group header={<Header mode="secondary">Items</Header>}>
+                                <SimpleCell>Hello</SimpleCell>
+                                <SimpleCell>World</SimpleCell>
+                            </Group>
+                        </Panel>
+                    </View>
+                </SplitCol>
+            </SplitLayout>
+        </AppRoot>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
+import { ConfigProvider, AdaptivityProvider } from '@vkontakte/vkui';
 import App from "./App";
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ConfigProvider>
+        <AdaptivityProvider>
+            <App />
+        </AdaptivityProvider>
+    </ConfigProvider>,
   document.getElementById("root")
 );
