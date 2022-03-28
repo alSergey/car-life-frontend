1e070322db41c459eeaaa691fdaef2e3b51fec23
Varvara Zabelina <varvara.zabelina@B-Q29VQ05P-0219.local>
2022-03-28 19:56:49 +0300

    new map

--- src/widgets/MapWidget/MapWidget.tsx
+++ src/widgets/MapWidget/MapWidget.tsx
@@ -2,7 +2,13 @@ import React, { useEffect, useRef, useState } from "react";
 import { Map, Placemark, YMaps } from "react-yandex-maps";
 import { getEventList } from "./api";
 import { emptyEventList } from "./api/api.consts";
-import { Panel } from "@vkontakte/vkui";
+import {
+	Avatar,
+	Group,
+	HorizontalCell,
+	HorizontalScroll,
+	Panel,
+} from "@vkontakte/vkui";
 import { YandexKey } from "../../constants/yandexKey";
 
 interface Props {
@@ -25,25 +31,28 @@ export const MapWidget: React.FC<Props> = ({ id }) => {
 	useEffect(() => {
 		handleGetEventList();
 	}, []);
+
+	function handleClickEvent() {
+		const map = myMap.current;
+		if (map) {
+			// @ts-ignore
+			map.action._map.setCenter([this.latitude, this.longitude]);
+			// @ts-ignore
+			map.action._map.setZoom(13);
+		}
+	}
+
 	return (
 		<Panel id={id}>
 			<YMaps query={YandexKey}>
 				<Map
 					defaultState={{
-						center: [55.751574, 37.573856],
+						center: [55.684758, 37.738521],
 						zoom: 11,
-					}}
-					onLoad={() => {
-						{
-							events.map((e) => (
-								<Placemark key={e.id} geometry={[e.latitude, e.longitude]} />
-							));
-						}
 					}}
 					width={"100%"}
 					height={"800px"}
 					options={{
-						yandexMapDisablePoiInteractivity: true,
 						suppressMapOpenBlock: true,
 					}}
 					instanceRef={(ref) => {
@@ -51,9 +60,44 @@ export const MapWidget: React.FC<Props> = ({ id }) => {
 						if (ref) myMap.current = ref;
 					}}
 				>
-					{events.map((e) => (
-						<Placemark key={e.id} geometry={[e.latitude, e.longitude]} />
-					))}
+					<Group
+						style={{
+							position: "absolute",
+							bottom: "44px",
+							zIndex: 1,
+							backgroundColor: "rgba(201,201,201,0.4)",
+							width: "100%",
+						}}
+					>
+						<HorizontalScroll
+							showArrows
+							getScrollToLeft={(i) => i - 120}
+							getScrollToRight={(i) => i + 120}
+						>
+							<div style={{ display: "flex" }}>
+								{events.map((e) => {
+									return (
+										<HorizontalCell
+											key={e.id}
+											header={e.name}
+											subtitle={e.event_date}
+											size="l"
+											onClick={handleClickEvent.bind(e)}
+										>
+											<Avatar size={128} mode="image" src={e.avatar} />
+											<Placemark
+												key={e.id}
+												geometry={[e.latitude, e.longitude]}
+											/>
+										</HorizontalCell>
+									);
+								})}
+							</div>
+						</HorizontalScroll>
+					</Group>
+					{/* {events.map((e) => ( */}
+					{/* 		<Placemark key={e.id} geometry={[e.latitude, e.longitude]} /> */}
+					{/* ))} */}
 				</Map>
 			</YMaps>
 		</Panel>
