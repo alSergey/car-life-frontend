1d5408a6be4c5828df297eb19a9447dd0cd3657c
Alekhin Sergey <alekhin1991@gmail.com>
2022-05-03 21:56:18 +0300

    fix: рефактор мини евентов

---
+++ src/widgets/MapPeopleWidget/CreateMiniEventForm/CreateMiniEventForm.config.ts
@@ -0,0 +1,14 @@
+export const EventsTypes = [
+	{
+		value: 1,
+		label: "Нужна помощь",
+	},
+	{
+		value: 2,
+		label: "Мини-сходка",
+	},
+	{
+		value: 3,
+		label: "Опасность",
+	},
+];
--- src/widgets/MapPeopleWidget/api/api.ts
+++ src/widgets/MapPeopleWidget/api/api.ts
@@ -1,28 +1,5 @@
-import {
-	ModelsCreateMiniEventRequest,
-	ModelsMiniEvent,
-} from "../../../api/Api";
+import { ModelsMiniEvent } from "../../../api/Api";
 import { api } from "../../../api";
-import { isMiniEventFormFilled } from "./api.utils";
-import { convertMiniEventTimeToUTC } from "../../../constants/time";
 
-export const getMiniEventList = (): Promise<ModelsMiniEvent[]> => {
-	return api.miniEvents.miniEventsList().then(({ data }) => data);
-};
-
-export const createNewMiniEvent = (
-	form: ModelsCreateMiniEventRequest
-): Promise<number | undefined> => {
-	if (!isMiniEventFormFilled(form) || !form.type_id)
-		throw new Error("Не заполнены все поля");
-
-	return api.miniEvent
-		.createCreate({
-			description: form.description,
-			type_id: form.type_id,
-			ended_at: convertMiniEventTimeToUTC(form.ended_at),
-			latitude: form.latitude,
-			longitude: form.longitude,
-		})
-		.then(({ data }) => data.id);
-};
+export const getMiniEventList = (): Promise<ModelsMiniEvent[]> =>
+	api.miniEvents.miniEventsList().then(({ data }) => data);
